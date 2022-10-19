83d9b85b22bec0221fbc6888958b64628ebfd5b9
MohamedTawfik97 <57731258+MohamedTawfik97@users.noreply.github.com>
2022-10-19 13:50:21 +0200

    Update webinspect_automation.ps1

--- webinspect_automation.ps1
+++ webinspect_automation.ps1
@@ -1,31 +1,31 @@
-﻿#Written for Powershell 7
+powershell '''#Written for Powershell 7
 #Variables
-$wiAPIurl = '192.168.10.161:8083'
-$sscURL = 'http://192.168.10.224:8080/ssc/'
-$applicationVersionID = '10001'
-$sscToken = 'NzFlOGJiMmMtYmFkMy00ZDI2LTk5NzQtMjdmZTQ0NmQzOTBk'
+$wiAPIurl = \'192.168.10.161:8083\'
+$sscURL = \'http://192.168.10.224:8080/ssc/\'
+$applicationVersionID = \'10001\'
+$sscToken = \'NzFlOGJiMmMtYmFkMy00ZDI2LTk5NzQtMjdmZTQ0NmQzOTBk\'
 
-$scanURL = 'http://zero.webappsecurity.com/'
-$crawlAuditMode = 'CrawlandAudit'
-$scanName = 'zero.webappsecurity'
+$scanURL = \'http://zero.webappsecurity.com/\'
+$crawlAuditMode = \'CrawlandAudit\'
+$scanName = \'zero.webappsecurity\'
 
 #API Auth
 $pwd = ConvertTo-SecureString "password1" -AsPlainText -Force
-$cred = New-Object Management.Automation.PSCredential ('username1', $pwd)
+$cred = New-Object Management.Automation.PSCredential (\'username1\', $pwd)
 
 #1. Run the scan
-$body = '{
+$body = \'{
 "settingsName": "Default",
 "overrides": {
-"scanName": "' + $scanName + '",
+"scanName": "\' + $scanName + \'",
 "startUrls": [
-"' + $scanURL + '"
+"\' + $scanURL + \'"
 ],
-"crawlAuditMode": "' + $crawlAuditMode + '",
+"crawlAuditMode": "\' + $crawlAuditMode + \'",
 "startOption": "Url"
-}'
+}\'
 
-$responseurl = 'http://' + $wiAPIurl + '/webinspect/scanner/scans'
+$responseurl = \'http://\' + $wiAPIurl + \'/webinspect/scanner/scans\'
 $response = Invoke-RestMethod -AllowUnencryptedAuthentication -Credential $cred -Method POST -ContentType "application/json" -Body $body -uri $responseurl
  
 #Store unique ScanId
@@ -33,7 +33,7 @@ $scanId = $response.ScanId
 Write-Host -ForegroundColor Green ("Scan started succesfully with Scan Id: " + $scanId)
  
 #2. Get the current Status of the Scan
-$StatusUrl = 'http://' + $wiAPIurl + '/webinspect/scanner/scans/' + $scanId + '/log'
+$StatusUrl = \'http://\' + $wiAPIurl + \'/webinspect/scanner/scans/\' + $scanId + \'/log\'
 $ScanCompleted = "ScanCompleted"
 $ScanStopped = "ScanStopped"
 $ScanInterrupted = "ScanInterrupted"
@@ -53,21 +53,21 @@ if ($ScanStatus -eq $ScanCompleted){
     Write-Host -ForegroundColor Green ("Scan completed!") `n
 
     #3. Export the scan to the FPR format
-    $fprurl = 'http://' + $wiAPIurl + '/webinspect/scanner/scans/' + $scanId + '.fpr '
-    $path = $scanId + '.fpr'
+    $fprurl = \'http://\' + $wiAPIurl + \'/webinspect/scanner/scans/\' + $scanId + \'.fpr \'
+    $path = $scanId + \'.fpr\'
 
     Write-Host ("Downloading the result file (fpr)...")
     Invoke-RestMethod -AllowUnencryptedAuthentication -Credential $cred -Method GET -OutFile $path -uri "$fprurl"
     Write-Host -ForegroundColor Green ("Result file (fpr) download done!") `n
  
     #4. Upload the Results to SSC
-    $sscheaders = '@{
-        "Authorization" = "FortifyToken '+ $sscToken + '"
+    $sscheaders = \'@{
+        "Authorization" = "FortifyToken \'+ $sscToken + \'"
         "ContentType" = "multipart/form-data"
         "accept" = "application/json"
-    }'
+    }\'
     $sscheader_exp = Invoke-Expression $sscheaders
-    $sscuploadurl = $sscURL + 'api/v1/projectVersions/' + $applicationVersionID + '/artifacts'
+    $sscuploadurl = $sscURL + \'api/v1/projectVersions/\' + $applicationVersionID + \'/artifacts\'
 
     Write-Host ("Starting Upload to SSC...")
     Invoke-RestMethod -uri $sscuploadurl -Method POST -Headers $sscheader_exp -Form @{file=(Get-Item $path)}
@@ -75,4 +75,4 @@ if ($ScanStatus -eq $ScanCompleted){
 }
 else {
     Write-Host -ForegroundColor Red ("Error occured after Scan was finished!")
-}
+}'''
